b499fb34227ae41c8e641d682972a3373c651418
Diogo Gomes <diogogomescastro@outlook.com>
2024-12-14 01:53:36 -0300

    fix final

--- frontend/src/components/CardItemPedido.tsx
+++ frontend/src/components/CardItemPedido.tsx
@@ -3,7 +3,7 @@ import ItemPedidoOutput from "@/DTOs/outputs/ItemPedidoOutput";
 export function CardItemPedido({ itemPedido }: { itemPedido: ItemPedidoOutput }) {
     return (
         <div className="border border-black mt-4 flex items-center sm:w-[55%] w-[80%] rounded-lg hover:bg-gray-50">
-            <img src={itemPedido.itemCardapioOutput.fotoUrl} className="rounded-l-lg sm:w-[140px] w-[90px]" width={140} height={140} alt={`Foto do pedido do item ${itemPedido.itemCardapioOutput.nome}`} />
+            <img src={itemPedido.itemCardapioOutput.fotoUrl} className="rounded-l-lg sm:w-[144px] w-[90px]" width={140} height={140} alt={`Foto do pedido do item ${itemPedido.itemCardapioOutput.nome}`} />
             <div className="text-start sm:pl-4 pl-2 sm:text-base text-[9px]">
                 <h2 className="font-semibold">{itemPedido.itemCardapioOutput.nome}</h2>
                 <p>{`Descrição: ${itemPedido.itemCardapioOutput.descricao}`}</p>
